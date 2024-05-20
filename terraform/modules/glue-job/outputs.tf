7411eda767331e7d2f61f10f3a4c3f4053901f0a
HC <60735042+hannahcass@users.noreply.github.com>
2024-05-20 20:46:21 +0200

    debug

--- terraform/modules/glue-job/outputs.tf
+++ terraform/modules/glue-job/outputs.tf
@@ -1,14 +1,9 @@
 output "job_arn" {
   description = "Amazon Resource Name (ARN) of Glue Job."
-  value       = aws_glue_job.this.arn
+  value       = aws_glue_job.glue_job.arn
 }
 
 output "job_id" {
   description = "Job name."
-  value       = aws_glue_job.this.id
-}
-
-output "job_role_arn" {
-  description = "Amazon Resource Name (ARN) specifying the role."
-  value       = length(var.role_arn) > 0 ? var.role_arn : join("", aws_iam_role.role.*.arn)
+  value       = aws_glue_job.glue_job.id
 }
